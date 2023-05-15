941cc8c990fb5667b4dfd6862e408aa3d3c1b701
1909Mohit <mohit1909.mk@gmail.com>
2023-05-15 13:50:22 +0530

    v1.0.0

---
+++ server.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
+import uploadRouter from './routes/uploadRoutes.js';
+
+dotenv.config();
+const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/amazona';
+mongoose
+  .connect(dbUrl,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+app.get('/api/keys/google', (req, res) => {
+  res.send({ key: process.env.GOOGLE_API_KEY || '' });
+});
+
+app.use('/api/upload', uploadRouter);
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
